fa6b53201bcd75512b14dc88e704d8a1bd68f057
Yashveer Sekhon <73847249+Yashveer1212@users.noreply.github.com>
2023-02-22 04:53:17 -0800

    Update QueueTest.java

--- Test/QueueTest.java
+++ Test/QueueTest.java
@@ -6,41 +6,48 @@ class QueueTest {
 
     //Tests for length
     @Test
-    void lengthShouldBe1() {
+    void checklength1() {
         Queue queue = new Queue();
         queue.addToBack(100);
-        assertEquals(1, queue.length());
+        assertEquals(1, queue.length());//length should be 1 to pass this test
     }
 
     @Test
-    void lengthShouldBe2() {
-        Queue queue = new Queue();
-        queue.addToBack(100);
-        queue.addToBack(99);
-        assertEquals(2, queue.length());
-    }
-
-    @Test
-    void lengthShouldBe3() {
+    void checklength3() {
         Queue queue = new Queue();
         queue.addToBack(100);
         queue.addToBack(99);
         queue.addToBack(98);
-
-        assertEquals(3, queue.length());
+        assertEquals(3, queue.length());//length should be 3 to pass this test
     }
 
-//Tests for adding integer to the q
+    @Test
+    void checklength9() {
+        Queue queue = new Queue();
+        queue.addToBack(100);
+        queue.addToBack(99);
+        queue.addToBack(98);
+        queue.addToBack(97);
+        queue.addToBack(96);
+        queue.addToBack(95);
+        queue.addToBack(94);
+        queue.addToBack(93);
+        queue.addToBack(92);
+
+        assertEquals(9, queue.length());//length should be 9 to pass this test
+    }
+
+//In these tests below, I will test by adding integers back into the queue in various ways
 
     @Test
-    void add0tobackof() {
+    void add0toBackofQueue() {
         Queue queue = new Queue();
 
         assertEquals("Empty List!", queue.printOut());
     }
 
     @Test
-    void add1toback() {
+    void add1toBackofQueue() {
         Queue queue = new Queue();
         queue.addToBack(1);
 
@@ -48,16 +55,7 @@ class QueueTest {
     }
 
     @Test
-    void add2toback() {
-        Queue queue = new Queue();
-        queue.addToBack(1);
-        queue.addToBack(2);
-
-        assertEquals("1\n-\n2\n-\n", queue.printOut());
-    }
-
-    @Test
-    void add5toback() {
+    void add5toBackofQueue() {
         Queue queue = new Queue();
         queue.addToBack(1);
         queue.addToBack(2);
@@ -66,12 +64,12 @@ class QueueTest {
         queue.addToBack(5);
 
 
-        assertEquals("1\n-\n2\n-\n3\n-\n4\n-\n5\n-\n", queue.printOut());
+        assertEquals("1\n-\n2\n-\n3\n-\n4\n-\n5\n-\n", queue.printOut());// queue be 5 integers long
     }
 
-    //Tests for removeFromFront method
+    //Tests below will test various ways integers are placed and taken away from the queue
     @Test
-    void remove1FromFront() {
+    void removeFromFront1() {
         Queue queue = new Queue();
         queue.addToBack(1);
         queue.addToBack(2);
@@ -79,14 +77,14 @@ class QueueTest {
         queue.addToBack(4);
         queue.addToBack(5);
 
-        queue.removeFromFront();
+        queue.removeFromFront();// should remove 1
 
 
-        assertEquals("2\n-\n3\n-\n4\n-\n5\n-\n", queue.printOut());
+        assertEquals("2\n-\n3\n-\n4\n-\n5\n-\n", queue.printOut());// queue should start with 2
     }
 
     @Test
-    void remove2FromFront() {
+    void removeFromFront2() {
         Queue queue = new Queue();
         queue.addToBack(1);
         queue.addToBack(2);
@@ -94,15 +92,15 @@ class QueueTest {
         queue.addToBack(4);
         queue.addToBack(5);
 
-        queue.removeFromFront();
-        queue.removeFromFront();
+        queue.removeFromFront();// should remove 1
+        queue.removeFromFront();// should remove 2
 
 
-        assertEquals("3\n-\n4\n-\n5\n-\n", queue.printOut());
+        assertEquals("3\n-\n4\n-\n5\n-\n", queue.printOut());// queue should start with 3
     }
 
     @Test
-    void remove3FromFront() {
+    void removeFromFront3() {
         Queue queue = new Queue();
         queue.addToBack(1);
         queue.addToBack(2);
@@ -110,41 +108,11 @@ class QueueTest {
         queue.addToBack(4);
         queue.addToBack(5);
 
-        queue.removeFromFront();
-        queue.removeFromFront();
-        queue.removeFromFront();
+        queue.removeFromFront();// should remove 1
+        queue.removeFromFront();// should remove 2
+        queue.removeFromFront();// should remove 3
 
 
-        assertEquals("4\n-\n5\n-\n", queue.printOut());
-    }
-
-//Tests for length
-
-    @Test
-    void queueWith3substract1() {
-        Queue queue = new Queue();
-
-        queue.addToBack(100);
-        queue.addToBack(99);
-        queue.addToBack(98);
-        queue.removeFromFront();
-
-        assertEquals(2, queue.length());
-
-    }
-
-
-    @Test
-    void queueWith3substract1add1() {
-        Queue queue = new Queue();
-        queue.addToBack(100);
-        queue.addToBack(99);
-        queue.addToBack(98);
-
-        queue.removeFromFront();
-
-        queue.addToBack(100);
-
-        assertEquals(3, queue.length());
+        assertEquals("4\n-\n5\n-\n", queue.printOut());// queue should start with 4
     }
 }
